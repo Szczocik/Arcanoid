8ccd08e3fc95c102b0c5d879088b6cd03b62668a
Mariusz Popa <popa.mariusz@gmail.com>
2022-06-08 04:31:30 -0400

    Bricks

--- Levels/GameState.gd
+++ Levels/GameState.gd
@@ -12,7 +12,13 @@ func _ready():
 func _on_Bottom_body_entered(body):
 	$Ball.position = Vector2(960, 540)
 	Ball -= 1
-	if Ball <= 0:
-		pass
+	if Ball < 0:
+		game_over()
 		
+		
+		
+		
+func game_over():
+	get_tree().change_scene("res://Levels/Game_over.tscn")
+	
 	
