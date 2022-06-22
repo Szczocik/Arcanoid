d15f872fc37dbe8dd83b46e60971a2c9b35c698e
Mariusz Popa <popa.mariusz@gmail.com>
2022-06-22 05:17:23 -0400

    script Ball_9

---
+++ Ball/Ball_8.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+var speed = 750
+var velocity = Vector2.ZERO
+
+
+func _ready():
+	randomize()
+	velocity.x = [-0.8,0.8][randi() % 2]
+	velocity.y = [-1,1][randi() % 2]
+
+	
+func _physics_process(delta):
+	var collision_object = move_and_collide(velocity * speed * delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.normal)
+		$CollisionSound.play()
+		if collision_object.collider.is_in_group("bricks"):
+			collision_object.collider.queue_free()
+			get_tree().call_group("Gamestate", "brick_up")
+		if collision_object.collider.is_in_group("heart"):
+			collision_object.collider.queue_free()
+			get_tree().call_group("Gamestate", "ball_up")
+			$HeartSound.play()
+
+
+func stop_ball():
+	speed = 0
+	
+
+	
+
+func restart_ball():
+	speed = 750
+	velocity.x = [-0.8,0.8][randi() % 2]
+	velocity.y = [-1,1][randi() % 2]
